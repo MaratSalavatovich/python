91c369ca921fcd04f52bae8b0deb82846f62f662
Marat <msshayapov@gmail.com>
2023-03-18 03:55:58 +0100

    Solution task_01_02_03_04

--- homework_01/task_03.py
+++ homework_01/task_03.py
@@ -8,6 +8,7 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
 number = int(input("Введите число n- "))
 number_sum = (number + int(str(number) + str(number)) + int(
     str(number) + str(number) + str(number)))
